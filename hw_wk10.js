4de9c9fc001f7542b37b5eb23ae42aa51bb422c5
jsoh0011 <102638980+jsoh0011@users.noreply.github.com>
2023-10-06 22:19:42 +0800

    uploading files for homework week 10

---
+++ hw_wk10.js
@@ -0,0 +1,6 @@
+  var vis = "hw_wk10.vg.json";
+
+  vegaEmbed('#vis', vis).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+
